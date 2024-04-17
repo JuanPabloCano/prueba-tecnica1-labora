862b3119fbfaf17520944b3fa320bc1e038115cd
Juan Pablo Cano <canojuan0415@gmail.com>
2024-04-17 02:14:46 -0500

    Chore: add generic string scanner

--- bug-tracking-system/shared/scanner.go
+++ bug-tracking-system/shared/scanner.go
@@ -1,9 +1,21 @@
 package shared
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func GetScanner() int {
 	var choice int
 	fmt.Scanln(&choice)
+	return choice
+}
+
+func GetScannerString() string {
+	reader := bufio.NewReader(os.Stdin)
+	choice, _ := reader.ReadString('\n')
+	choice = strings.TrimSpace(choice)
 	return choice
 }
